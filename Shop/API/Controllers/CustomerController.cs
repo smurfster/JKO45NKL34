560e56358e4f0915f82eb9a50b936b751582ec18
SteveParry <stevedparry@outlook.com>
2024-01-21 14:43:35 +0000

    Get_OnSuccess_ReturnStatusCode200() test failed

---
+++ Shop/API/Controllers/CustomerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        public async Task<IActionResult> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Shop/UnitTests/Controllers/CustomerControllerTests.cs
@@ -0,0 +1,19 @@
+using API.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UnitTests.Controllers
+{
+    public class CustomerControllerTests
+    {
+        [Fact]
+        public async void Get_OnSuccess_ReturnStatusCode200()
+        {
+            var sut = new CustomerController();
+
+            var result = (OkObjectResult)await sut.Get(1);
+
+            result.StatusCode.Should().Be(200);
+        }
+    }
+}
