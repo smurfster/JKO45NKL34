05e91638a0983074de5538cb0191f60c6c996f9c
SteveParry <stevedparry@outlook.com>
2024-01-21 21:56:07 +0000

    Update_OnSuccess_InvokesCustomerServiceExactlyOnce() passing

--- Shop/API/Controllers/CustomerController.cs
+++ Shop/API/Controllers/CustomerController.cs
@@ -40,7 +40,8 @@ namespace API.Controllers
 
         [HttpPut]
         public async Task<IActionResult> Update(CreateUpdateCustomerRequestModel customer)
-        {            
+        {
+            await customerService.Update(customer);
             return NoContent();
         }
     }
