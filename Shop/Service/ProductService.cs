0b8e500648845d8582d9615c9eb977bb54de2dae
SteveParry <stevedparry@outlook.com>
2024-01-22 00:38:47 +0000

    CreateProduct_OnSuccess_ReturnId() pass

--- Shop/Service/ProductService.cs
+++ Shop/Service/ProductService.cs
@@ -24,7 +24,7 @@ namespace Service
             var product = await productRepository.CreateProduct(entity);
             
             dbContext.SaveChanges();
-            return -1;
+            return product.Id;
         }
     }
 }
