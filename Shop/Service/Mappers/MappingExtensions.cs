2d85c012ab1239bf455ce32cefadf1bc9d393321
SteveParry <stevedparry@outlook.com>
2024-01-22 00:26:50 +0000

    CreateProduct_WhenCalled_InvokesCustomerRepositoryAndDbContextMock() pass

---
+++ Shop/Service/Mappers/MappingExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Entities.Customer;
+using Domain.Entities.Product;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Mappers
+{
+    internal static class MappingExtensions
+    {
+        internal static GetCustomerResponseModel CustomerEntityToGetCustomerResponseModel
+            (this CustomerEntity entity)
+        {
+            return new GetCustomerResponseModel()
+            {
+                Id = entity.Id,
+                Email = entity.Email,
+                Name = entity.Name,
+                Phone = entity.Phone
+            };
+        }
+
+        internal static CustomerEntity CreateUpdateCustomerRequestModelToCustomerEntity(this CreateUpdateCustomerRequestModel model)
+        {
+            return new CustomerEntity(model.Name, model.Email, model.Phone);
+        }
+
+        internal static ProductEntity CreateUpdateProductRequestModelToProductEntity(this CreateUpdateProductRequestModel model)
+        {
+            return new ProductEntity(model.Name, model.Description, SKU.Create(model.Sku)!);
+        }
+    }
+}
